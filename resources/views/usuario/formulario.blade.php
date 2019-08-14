08fc8a6c37d1040da3e36a7283a234db41cec914
ENDERSONYUNDASANCHEZ <53826353+ENDERSONYUNDASANCHEZ@users.noreply.github.com>
2019-08-13 20:49:02 -0500

    cambios

---
+++ resources/views/usuario/formulario.blade.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+ <p>nombre: <input type="text" name="nombre" /></p>
+ <p>edad: <input type="text" name="edad" /></p>
+ <p><input type="submit" /></p>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('usuario.welcome');
 });
+
+Route::get('/usuario', function () {
+    return view('usuario.create');
+});
+
+Route::get('/usuario', function () {
+    return view('usuario.formulario');
+});
+*/
+Route::get('/eje1', function () {
+    return view('usuario.ejercicio1');
+});
+
+Route::get('/eje2', function () {
+    return view('usuario.ejercicio2');
+});
+
+Route::get('/eje3', function () {
+    return view('usuario.ejercicio3');
+});
+
+Route::resource('usuario', 'UsuarioController');
